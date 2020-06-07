c2fcb6a1524d0eb9eb90169036efcf398ca40376
adamzvx <adammmfs@gmail.com>
2020-06-07 19:55:21 +0700

    update bootestrap

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <h1>This is an profile page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="media">
+            <img class="mr-3" src="..." alt="Generic placeholder image">
+            <div class="media-body">
+              <h5 class="mt-0">Media heading</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 export default {
   name: 'Profile',
